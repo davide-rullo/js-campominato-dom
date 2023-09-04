f25b3099de6f0ffff6ab4ac4655d3127837afd49
davide-rullo <davide.rullo01@gmail.com>
2023-09-04 13:47:57 +0200

    add
    added script to list only unique numbers

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,38 +6,57 @@ const difficulty = document.getElementById("levels")
 let bombs = [];
 playBtn.addEventListener("click", function () {
 
-    
+
 
     let levels = difficulty.value;
 
     if (levels === "1") {
         createTable(gameTable, 100);
+
         
-        for (let i = 1; i <= 16; i++) {
-            let randomNumber = Math.floor(Math.random() * 100) + 1;
+        while (bombs.length < 16) {
+            let randomNumber;
+            do {
+                randomNumber = Math.floor(Math.random() * 100) + 1;
+            } while (bombs.includes(randomNumber));
+
             bombs.push(randomNumber);
-            
         }
-        
+
+        console.log(bombs);
+
 
     } else if (levels === "2") {
         createTable(gameTable, 81);
-       
-        for (let i = 1; i <= 16; i++) {
-            let randomNumber = Math.floor(Math.random() * 81) + 1;
+
+
+        while (bombs.length < 16) {
+            let randomNumber;
+            do {
+                randomNumber = Math.floor(Math.random() * 81) + 1;
+            } while (bombs.includes(randomNumber));
+
             bombs.push(randomNumber);
-            
         }
-        
+
+        console.log(bombs);
+
 
     } else if (levels === "3") {
         createTable(gameTable, 49);
+
         
-        for (let i = 1; i <= 16; i++) {
-            let randomNumber = Math.floor(Math.random() * 49) + 1;
+        while (bombs.length < 16) {
+            let randomNumber;
+            do {
+                randomNumber = Math.floor(Math.random() * 49) + 1;
+            } while (bombs.includes(randomNumber));
+
             bombs.push(randomNumber);
-            
         }
+
+        console.log(bombs);
+
     }
 });
 
@@ -45,14 +64,14 @@ playBtn.addEventListener("click", function () {
 function createTable(DOMelement, maxSquareNumber) {
     for (let i = 0; i < maxSquareNumber; i++) {
         const square = document.createElement("div");
-       
+
         square.className = "square";
-        square.append(i+1);
+        square.append(i + 1);
         gameTable.append(square);
 
         playBtn.classList.add("d-none");
         difficulty.classList.add("d-none");
-        
+
         square.addEventListener("click", function () {
             square.classList.toggle("green-square");
             console.log("Clic sulla casella " + (i + 1));
